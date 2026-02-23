ee16e6bb9a4d4abceac256ae450fef4168b68b41
marcosmartincmg-dot <marcos.martin.cmg@gmail.com>
2026-02-23 20:10:10 +0100

    Update Aplicacion.java

--- Aplicacion.java
+++ Aplicacion.java
@@ -1,6 +1,6 @@
 public class Aplicacion {
     public static void main(String[] args) {
         System.out.println("Hola, esta es la aplicacion principal.");
-        System.System.out.println("Modificación por Marcos Martín");
+        System.out.println("Modificación por Marcos Martín");
     }
 }
